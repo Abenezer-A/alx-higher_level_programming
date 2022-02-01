22c90ed4b55b4bc732b78200faf6a59e01709501
Cepheus-king <84628709+cepheus-king@users.noreply.github.com>
2022-02-01 09:12:45 -0800

    Update 4-starwars_count.js

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,22 +1,17 @@
 #!/usr/bin/node
 const request = require('request');
-const url = process.argv[2];
-request(url, function (err, response, body) {
-  if (err) {
-    console.log(err);
-  } else if (response.statusCode === 200) {
-    let films = JSON.parse(body).results;
-    let count = 0;
-    for (let filmIndex in films) {
-      let filmChars = films[filmIndex].characters;
-      for (let charIndex in filmChars) {
-        if (filmChars[charIndex].includes('18')) {
-          count++;
-        }
-      }
-    }
-    console.log(count);
-  } else {
-    console.log('An error occured. Status code: ' + response.statusCode);
+const { argv } = require('process');
+
+request(argv[2], (error, response, body) => {
+  if (error) {
+    console.error(error);
   }
+  let Count = 0;
+  const doc = JSON.parse(body).results;
+  doc.forEach(p => p.characters.forEach(msg => {
+    if (msg.slice(-3, -1) === '18') {
+      Count++;
+    }
+  }));
+  console.log(Count);
 });
