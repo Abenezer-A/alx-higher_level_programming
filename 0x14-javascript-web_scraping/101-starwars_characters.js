a6c94ac34662e78710f488b1335a276666eb1f73
Cepheus-king <84628709+cepheus-king@users.noreply.github.com>
2022-02-01 09:18:50 -0800

    Update 101-starwars_characters.js

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,28 +1,27 @@
 #!/usr/bin/node
 const request = require('request');
-const { argv } = require('process');
-
-const BaseUrl = 'https://swapi-api.hbtn.io/api/films/';
-function MakeRequest (url) {
-  return new Promise(function (resolve, reject) {
-    request(url, (error, response, body) => {
-      if (!error && response.statusCode === 200) {
-        resolve(body);
-      } else {
-        reject(error);
+const film = process.argv[2];
+let url = 'http://swapi.co/api/people/';
+function filmcharacters (film, url) {
+  request(url, function (err, response, body) {
+    if (err) {
+      console.log(err);
+    } else if (response.statusCode === 200) {
+      let jsonobj = JSON.parse(body);
+      let people = jsonobj.results;
+      for (let i in people) {
+        for (let j in people[i].films) {
+          if (people[i].films[j].includes(film)) {
+            console.log(people[i].name);
+          }
+        }
       }
-    });
+      if (jsonobj.next !== null) {
+        filmcharacters(film, jsonobj.next);
+      }
+    } else {
+      console.log('An error occured. Status code: ' + response.statusCode);
+    }
   });
 }
-
-async function main () {
-  const movie = await MakeRequest(BaseUrl + argv[2]);
-  const characters = JSON.parse(movie).characters;
-  characters.forEach(async function (element) {
-    const character = await MakeRequest(element);
-    const CharacterName = JSON.parse(character).name;
-    console.log(CharacterName);
-  });
-}
-
-main();
+filmcharacters(film, url);
