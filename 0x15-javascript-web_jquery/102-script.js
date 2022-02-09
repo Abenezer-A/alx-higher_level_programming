15989a964fe93fa2b6f2a639e9fa7755f7795085
Cepheus-king <84628709+cepheus-king@users.noreply.github.com>
2022-02-09 12:09:59 -0800

    Update 102-script.js

--- 0x15-javascript-web_jquery/102-script.js
+++ 0x15-javascript-web_jquery/102-script.js
@@ -1,9 +1,9 @@
 $(function () {
-  const MyButton = $('#btn_translate');
+  const MyButton = $('INPUT#btn_translate');
   MyButton.click(function () {
-    const InputLang = $('#language_code').val();
-    $.get('https://fourtonfish.com/hellosalut/', { lang: InputLang }, (data, status) => {
-      $('div#hello').text(data.hello);
+    const InputLang = $('INPUT#language_code').val();
+    $.get('https://fourtonfish.com/hellosalut/hello/', { lang: InputLang }, (data, status) => {
+      $('DIV#hello').text(data.hello);
     });
   });
 });
